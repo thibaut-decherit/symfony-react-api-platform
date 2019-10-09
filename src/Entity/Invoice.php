a227c7f0f6ac303a5f2567adc248f55b471c2aaa
Thibaut Decherit <tdecherit@entreloups.com>
2019-10-09 15:04:50 +0200

    feat(api): add order by desc amount on Invoice

--- src/Entity/Invoice.php
+++ src/Entity/Invoice.php
@@ -12,6 +12,7 @@ use Doctrine\ORM\Mapping as ORM;
  *     attributes={
  *          "pagination_enabled"=true,
  *          "pagination_items_per_page"=20,
+ *          "order"={"amount"="desc"},
  *     }
  * )
  */

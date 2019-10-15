c00f22f7b132c06216f4bb3be8af2c7cf0a16f4d
Thibaut Decherit <tdecherit@entreloups.com>
2019-10-15 14:58:49 +0200

    feat(api): link current User to Customer object on new Customer POST

--- src/EventSubscriber/KernelViewSubscriber.php
+++ src/EventSubscriber/KernelViewSubscriber.php
@@ -3,11 +3,13 @@
 namespace App\EventSubscriber;
 
 use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Entity\Customer;
 use App\Entity\User;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ViewEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Security;
 
 /**
  * Class KernelViewSubscriber
@@ -21,12 +23,19 @@ class KernelViewSubscriber implements EventSubscriberInterface
     private $passwordEncoder;
 
     /**
+     * @var Security
+     */
+    private $security;
+
+    /**
      * KernelViewSubscriber constructor.
      * @param UserPasswordEncoderInterface $passwordEncoder
+     * @param Security $security
      */
-    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder, Security $security)
     {
         $this->passwordEncoder = $passwordEncoder;
+        $this->security = $security;
     }
 
     /**
@@ -37,22 +46,48 @@ class KernelViewSubscriber implements EventSubscriberInterface
         // See https://api-platform.com/docs/core/events/#the-event-system for priorities.
         return [
             KernelEvents::VIEW => [
-                ['hashPassword', EventPriorities::PRE_WRITE]
+                ['onCustomerPostSetUser', EventPriorities::PRE_VALIDATE],
+                ['onUserPostHashPassword', EventPriorities::PRE_WRITE]
             ]
         ];
     }
 
     /**
-     * Hash password of User before persistence in database.
-     *
      * @param ViewEvent $event
      */
-    public function hashPassword(ViewEvent $event): void
+    public function onCustomerPostSetUser(ViewEvent $event): void
     {
         $result = $event->getControllerResult();
         $method = $event->getRequest()->getMethod();
 
-        if ($result instanceof User !== false && $method !== 'POST') {
+        if ($result instanceof Customer === false || $method !== 'POST') {
+            return;
+        }
+
+        /**
+         * @var Customer $customer
+         */
+        $customer = $result;
+
+        /**
+         * @var User $user
+         */
+        $user = $this->security->getUser();
+
+        $customer->setUser($user);
+    }
+
+    /**
+     * Hashes password of User before persistence in database.
+     *
+     * @param ViewEvent $event
+     */
+    public function onUserPostHashPassword(ViewEvent $event): void
+    {
+        $result = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod();
+
+        if ($result instanceof User === false || $method !== 'POST') {
             return;
         }
 
